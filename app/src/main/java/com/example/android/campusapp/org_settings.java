b0f84b86da42a208cea98d47df660812c280e806
Fridakornsater <Frida@nl119-181-9.student.uu.se>
2017-04-07 12:37:31 +0200

    Settings for Organization

--- app/src/main/java/com/example/android/campusapp/org_settings.java
+++ app/src/main/java/com/example/android/campusapp/org_settings.java
@@ -1,8 +1,21 @@
 package com.example.android.campusapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by elsabergman on 2017-04-07.
  */
 
-public class org_settings {
+public class org_settings extends SlidingMenuActivity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.org_settings);
+
+
+    }
+
 }
