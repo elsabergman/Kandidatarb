d071f8a0e1192ea0656170e6f5ddb90ee54d3637
elisabethanna <anna.eriksson94@hotmail.com>
2017-04-10 13:42:26 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	local.properties

--- app/src/main/java/com/example/android/campusapp/SlidingMenuActivity.java
+++ app/src/main/java/com/example/android/campusapp/SlidingMenuActivity.java
@@ -62,7 +62,7 @@ public class SlidingMenuActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sliding_menu, menu);
+        getMenuInflater().inflate(R.menu.search_bar, menu);
         return true;
     }
 
@@ -74,7 +74,7 @@ public class SlidingMenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
