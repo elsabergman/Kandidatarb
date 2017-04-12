5badfae820d8215cc753ed99bd3bc805e2a7a15d
Fridakornsater <Frida@nl119-183-49.student.uu.se>
2017-04-12 09:13:15 +0200

    fridas

--- app/src/main/java/com/example/android/campusapp/org_settings.java
+++ app/src/main/java/com/example/android/campusapp/org_settings.java
@@ -1,21 +1,60 @@
 package com.example.android.campusapp;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
 /**
  * Created by elsabergman on 2017-04-07.
  */
 
 public class org_settings extends SlidingMenuActivity {
 
+    private TextView switchStatus;
+    private Switch mySwitch;
 
-    public void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.org_settings);
 
+        switchStatus = (TextView) findViewById(R.id.notifications);
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
 
+        //set the switch to ON
+        mySwitch.setChecked(true);
+        //attach a listener to check for changes in state
+        mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if(isChecked){
+                    switchStatus.setText("ON");
+                }else{
+                    switchStatus.setText("OFF");
+                }
+
+            }
+        });
+
+        //check the current state before we display the screen
+        if(mySwitch.isChecked()){
+            switchStatus.setText("ON");
+        }
+        else {
+            switchStatus.setText("OFF");
+        }
     }
+
+
+
+
 
 }
