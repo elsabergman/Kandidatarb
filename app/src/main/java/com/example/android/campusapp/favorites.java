9075e9cd12d78a511273b53ac0940e3cab4fa553
Fridakornsater <Frida@nl119-145-182.student.uu.se>
2017-04-19 09:39:59 +0200

    Favorites started

--- app/src/main/java/com/example/android/campusapp/favorites.java
+++ app/src/main/java/com/example/android/campusapp/favorites.java
@@ -1,22 +1,18 @@
 package com.example.android.campusapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by elsabergman on 2017-04-03.
  */
 
 public class favorites {
 
 
-    public class toggle_menu extends AppCompatActivity {
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.favorites);
-        }
+
 
 
     }
