aa2da7fc376ec5a0d7644cf8eeb7be0805cb8a24
Fridakornsater <Frida@nl119-145-182.student.uu.se>
2017-04-19 10:03:38 +0200

    Fridas commit på Arvids kod

--- app/src/main/java/com/example/android/campusapp/todays_events.java
+++ app/src/main/java/com/example/android/campusapp/todays_events.java
@@ -14,13 +14,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,6 +32,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 import static android.R.attr.button;
 
@@ -46,5 +50,30 @@ public class todays_events extends SlidingMenuActivity {
         View contentView = inflater.inflate(R.layout.todays_events, null);
         drawer.addView(contentView, 0);
 
+ /**   This part controls the spinner with checkboxes for choices,Code from Ironman post on stackexchange Jul 14 2016: http://stackoverflow.com/questions/38417984/android-spinner-dropdown-checkbox    */
+        final String[] select_qualification = {
+                "Choose filtering options", "Promoting event", "Lunch event", "Evening event", "Case event",
+                "Other"};
+        Spinner spinner = (Spinner) findViewById(R.id.filterSpinner);
+
+        ArrayList<StateVO> listVOs = new ArrayList<>();
+
+        for (int i = 0; i < select_qualification.length; i++) {
+            StateVO stateVO = new StateVO();
+            stateVO.setTitle(select_qualification[i]);
+            stateVO.setSelected(false);
+            listVOs.add(stateVO);
+        }
+        MyAdapter myAdapter = new MyAdapter(todays_events.this, 0,
+                listVOs);
+        spinner.setAdapter(myAdapter);
     }
+
+    public void onItemSelected (AdapterView< ? > parent, View view, int position, long id) {
+        //Här inne är vad som sker när en grej i listan väljs
+        Toast toast = Toast.makeText(todays_events.this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT);
+        toast.show();    /**Denna toast visar i en liten ruta vilken man valt*/
+    }
+
+
 }
