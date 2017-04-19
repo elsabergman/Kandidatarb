366910dd137606815ed39365d440303f0779c65f
Fridakornsater <f.kornsater@gmail.com>
2017-04-19 10:29:38 +0200

    Fridas synkning del 2

--- app/src/main/java/com/example/android/campusapp/login.java
+++ app/src/main/java/com/example/android/campusapp/login.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,16 +39,32 @@ import static android.R.attr.name;
 
 public class login extends Activity {
 
-    URL url;
+
     URLConnection urlConn;
     DataOutputStream printout;
     DataInputStream input;
+
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        TextView header = (TextView)findViewById(R.id.title);
+
+
+
+
+        Typeface custom_font = Typeface.createFromAsset(this.getAssets(),  "fonts/Shrikhand-Regular.ttf");
+
+        header.setTypeface(custom_font);
+
+        TextView at = (TextView)findViewById(R.id.at);
+
+        Typeface custom_font2 = Typeface.createFromAsset(this.getAssets(),  "fonts/Shrikhand-Regular.ttf");
+
+        at.setTypeface(custom_font2);
 
         loginFunctions();
         TextView createUser = (TextView) findViewById(R.id.createUser);
@@ -62,12 +79,14 @@ public class login extends Activity {
         });
     }
 
+
     void loginFunctions() {
         Button btn = (Button) findViewById(R.id.loginButton);
         TextView createUser = (TextView) findViewById(R.id.createUser);
         final EditText email_Edit = (EditText) findViewById(R.id.input_email);
         final EditText pwd_Edit = (EditText) findViewById(R.id.input_pwd);
         final TextView txtView = (TextView) findViewById(R.id.wrongInput);
+
 
         btn.setOnClickListener(new View.OnClickListener() {
 
@@ -80,6 +99,7 @@ public class login extends Activity {
 
                 JSONObject post_dict = new JSONObject();
 
+
                 try {
                     post_dict.put("username" , email);
                     post_dict.put("password", pwd);
@@ -89,19 +109,13 @@ public class login extends Activity {
                     e.printStackTrace();
                 }
                 if (post_dict.length() > 0) {
-                    System.out.println("i if sats");
-                    new SendToDatabase().execute(String.valueOf(post_dict));
+
+                    new SendToDatabase().execute( post_dict.toString(), "http://212.25.151.17:8000/auth/token/");
+
                 }
 
 
-               /* try {
-                    postData.put("username", email);
-                    postData.put("password", pwd);
 
-                    new SendToDatabase().execute("http://130.242.96.84:8010/users/login/", postData.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }*/
 
                 Intent intent = new Intent(login.this, org_my_events.class);
                 startActivity(intent);
@@ -109,40 +123,10 @@ public class login extends Activity {
             }
         });
     }
+
+
 }
 
-                                /*      if (("kandidat".equals(email)) && ("kand123".equals(pwd))) {
-
-
-                                           SharedPreferences sp = getSharedPreferences("Login", 0);
-                                           SharedPreferences.Editor Ed = sp.edit();
-                                           Ed.putString("Unm", email);
-                                           Ed.putString("Psw", pwd);
-                                           Ed.commit();
-
-                                           Intent intent = new Intent(login.this, org_my_events.class);
-                                           startActivity(intent);
-
-
-                                       } else {
-                                           txtView.setText("wrong password and/or email address");
-
-                                       }
-
-
-                                   }
-                               })*/
-
-
-
-
-
-
-
-
-
-// }
-//}
 
 
 
