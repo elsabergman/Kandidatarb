e13815847817f545384d4f732b7a7f2c802b245c
elisabethanna <anna.eriksson94@hotmail.com>
2017-04-21 09:55:56 +0200

    Create user färdigdesignad

--- app/src/main/java/com/example/android/campusapp/create_user.java
+++ app/src/main/java/com/example/android/campusapp/create_user.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 public class create_user extends AppCompatActivity {
@@ -12,34 +13,30 @@ public class create_user extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_user);
-        final RadioButton student_button = (RadioButton) findViewById(R.id.student_button);
-        final RadioButton org_button = (RadioButton) findViewById(R.id.organization_button);
+        final Button student_button = (Button) findViewById(R.id.create_stud_button);
+        final Button org_button = (Button) findViewById(R.id.create_organization_button);
 
-
-       student_button.setOnClickListener(new OnClickListener() {
-
+        student_button.setOnClickListener(new View.OnClickListener() {
 
         public void onClick(View v) {
-         if (student_button.isChecked()) {
-          Intent Intents = new Intent(create_user.this, create_student_user.class);
-             startActivity(Intents);
-             setContentView(R.layout.create_student_user);
-         }
-        }
-       });
+           Intent Intents = new Intent(create_user.this, create_student_user.class);
+           startActivity(Intents);
+            setContentView(R.layout.create_student_user);
+                 }
+
+            });
+
+            org_button.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    Intent Intents = new Intent(create_user.this, create_org_user.class);
+                    startActivity(Intents);
+                    setContentView(R.layout.create_org_user);
+                }
+            });
+    }
+}
 
 
-               org_button.setOnClickListener(new View.OnClickListener() {
 
-                   public void onClick(View v) {
-                       if (org_button.isChecked()) {
-                           Intent Intents = new Intent(create_user.this, create_org_user.class);
-                           startActivity(Intents);
-                           setContentView(R.layout.create_org_user);
-                       }
-                   }
 
-               });
-
-           }
-       }
