0f540546fc14e867bc0b7708dd02bcc15ee58103
elisabethanna <anna.eriksson94@hotmail.com>
2017-04-24 14:49:00 +0200

    GOING BACK TO WORKING GIT

--- app/src/main/java/com/example/android/campusapp/org_settings.java
+++ app/src/main/java/com/example/android/campusapp/org_settings.java
@@ -27,7 +27,6 @@ import static com.example.android.campusapp.R.id.organization_nameInput;
 import static com.example.android.campusapp.R.id.parent;
 
 import org.w3c.dom.Text;
-import static com.example.android.campusapp.R.id.campusesSpinner;
 import static com.example.android.campusapp.R.id.universitySpinnerSettings;
 
 
